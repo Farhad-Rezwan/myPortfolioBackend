ee7e617a64e2ea49a346ca3bd6523b549e4356e3
Farhad-Rezwan <farhad.rezwan@gmail.com>
2022-03-24 01:03:39 +1100

    Resolve ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,24 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
+        client: "postgres",
         host: config.host,
         port: config.port,
         database: config.database,
         username: config.user,
         password: config.password,
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
       },
       options: {
-        ssl: false,
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
